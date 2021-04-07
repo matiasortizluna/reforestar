269e6b4013793a219315452dcc558d91f51996e3
Matias Ariel <matiasarielortiz2001@gmail.com>
2021-04-08 00:05:55 +0100

    All main interfaces with ViewControllers

---
+++ reforestar/ProjectsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectsViewController.swift
+//  reforestar
+//
+//  Created by Matias Ariel Ortiz Luna on 08/04/2021.
+//
+
+import UIKit
+
+class ProjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
