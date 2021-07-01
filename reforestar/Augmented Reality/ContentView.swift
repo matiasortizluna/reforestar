a543a4e4a4980b1eff9e04512453c886d78baf97
Matias Ariel <matiasarielortiz2001@gmail.com>
2021-07-01 22:47:00 +0100

    d

--- reforestar/Augmented Reality/ContentView.swift
+++ reforestar/Augmented Reality/ContentView.swift
@@ -11,6 +11,10 @@ import ARKit
 import UIKit
 
 struct ContentView: View {
+<<<<<<< HEAD
+=======
+    @StateObject var placementSettings = PlacementSettings()
+>>>>>>> parent of 273822f (First Version of Algorithm)
     
     @State var heightValue : Double = 1.0
     @State var minimumValue : Double = 1.0
@@ -34,7 +38,9 @@ struct ContentView: View {
                 Spacer()
             }
             
+            
             HStack(alignment: .bottom){
+<<<<<<< HEAD
                 
                 //Interface AR
                 Spacer()
@@ -42,49 +48,107 @@ struct ContentView: View {
                 
             }
         }
+=======
+                //ARViewWrapper()
+                if(self.placementSettings.selectedModel == nil){
+                    //Interface AR
+                    Spacer()
+                    InterfaceLayout(heightValue: $heightValue, minimumValue: $minimumValue, maximumValue: $maximumValue)
+                }else{
+                    PlacementView()
+                }
+            }.environmentObject(placementSettings)
+        }
     }
+}
+
+
+struct ARViewWrapper: UIViewControllerRepresentable{
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+>>>>>>> parent of 273822f (First Version of Algorithm)
+    }
+    
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> ARView {
+    func makeUIView(context: Context) -> some UIView {
         
         let arView  = ARView(frame: .zero)
         
-        arView.setupARView()
-        arView.setupGestures()
+        arView.automaticallyConfigureSession = false;
+        let configuration = ARWorldTrackingConfiguration();
+        configuration.planeDetection = [.horizontal];
+        arView.session.run(configuration)
         
+<<<<<<< HEAD
+=======
+        arView.setupGestures()
+        /*
+         self.placementSettings.sceneObserver = arView.scene.subscribe(to: SceneEvents.Update.self, {(event) in
+         self.updateScene(for: arView)}
+         )*/
+        
+>>>>>>> parent of 273822f (First Version of Algorithm)
         return arView;
     }
     
     
-    func updateUIView(_ uiView: ARView
-                      , context: Context) {
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+    
+<<<<<<< HEAD
+}
+
+=======
+    public func updateScene(for arView: ARView){
+        if let confirmedModel = self.placementSettings.confirmedModel, let modelEntity = confirmedModel.modelEntity{
+            self.place(modelEntity, in: arView)
+            self.placementSettings.confirmedModel=nil
+        }
+    }
+    
+    private func place(_ modelEntity: ModelEntity, in arView: ARView){
+        let clonedEntity = modelEntity.clone(recursive: true)
+        
+        clonedEntity.generateCollisionShapes(recursive: true)
+        arView.installGestures(.all, for: clonedEntity)
+        
+        let anchorEntity = AnchorEntity(plane: .any)
+        anchorEntity.addChild(clonedEntity)
+        arView.scene.addAnchor(anchorEntity)
     }
     
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(PlacementSettings())
+    }
+}
+
+>>>>>>> parent of 273822f (First Version of Algorithm)
 extension ARView{
     
-    func setupARView(){
-        self.session.delegate = self
-        
-        self.automaticallyConfigureSession = false;
-        let configuration = ARWorldTrackingConfiguration();
-        configuration.planeDetection = [.horizontal,.vertical];
-        configuration.environmentTexturing = .automatic;
-        self.session.run(configuration)
-    }
-    
     func setupGestures() {
-        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+        self.session.delegate = self
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        self.addGestureRecognizer(tap)
     }
     
-    @objc
-    func handleTap(recognizer: UITapGestureRecognizer){
-        let touchInView = recognizer.location(in: self)
-        let results = self.raycast(from: touchInView, allowing: .estimatedPlane, alignment: .horizontal)
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        guard let touchInView = sender?.location(in: self) else {
+            print("Failed on touch")
+            return
+        }
         
+<<<<<<< HEAD
         if let firstResult = results.first{
             
             var positions = ReforestationSettings().getPositionsThreeDimension(from: firstResult.worldTransform, for: 20)
@@ -100,14 +164,20 @@ extension ARView{
                 
             }
             
+=======
+        let results = self.raycast(from: touchInView, allowing: .estimatedPlane, alignment: .horizontal)
+        if let firstResult = results.last{
+            let anchor = ARAnchor(name: "quercus_suber.usdz", transform: firstResult.worldTransform);
+            print(anchor.name)
+            print("First \(anchor.transform)")
+            self.session.add(anchor: anchor);
+            print("Second \(anchor.transform)")
+>>>>>>> parent of 273822f (First Version of Algorithm)
         }else{
             print("Object placement failed - coudn't find surface")
         }
-    }
-    
-    func placeObject(named entityName: String, for anchor: ARAnchor){
-        let entity = try! ModelEntity.load(named: entityName)
         
+<<<<<<< HEAD
         let anchorEntity = AnchorEntity(anchor: anchor)
         anchorEntity.addChild(entity);
         //anchorEntity.scale *= (1/100)
@@ -116,9 +186,27 @@ extension ARView{
         
         self.scene.anchors.append(anchorEntity)
         self.session.add(anchor: anchor)
+=======
         
-        entity.generateCollisionShapes(recursive: true)
-        //self.installGestures([.rotation,.translation], for: entity as! Entity & HasCollision)
+        /*
+         let coordinate = CLLocationCoordinate2D(latitude: 39.73954841, longitude: -8.80565608)
+         let geoAnchor = ARGeoAnchor(name: "quercus_suber.usdz", coordinate: coordinate)
+         print("Coordenadas: \(geoAnchor.coordinate.latitude)")
+         self.session.add(anchor: geoAnchor);
+         */
+        
+        /*
+         let point = SIMD3<Float>([0, 1, -2])
+         self.session.getGeoLocation(forPoint: point) {
+         (coordinate, altitude, error) in
+         let geoAnchor = ARGeoAnchor(name: "quercus_suber.usdz", coordinate: coordinate, altitude:
+         altitude)
+         print("Coordenadas: \(geoAnchor.coordinate.latitude)")
+         self.session.add(anchor: geoAnchor);
+         }
+         */
+>>>>>>> parent of 273822f (First Version of Algorithm)
+        
     }
     
 }
@@ -126,11 +214,27 @@ extension ARView{
 extension ARView: ARSessionDelegate{
     
     public func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        print("REached session")
         for anchor in anchors{
-            if let anchorName = anchor.name, anchorName == "quercus_suber"{
+            if let anchorName = anchor.name, anchorName == "quercus_suber.usdz"{
                 placeObject(named: anchorName, for: anchor)
             }
         }
     }
     
+    func placeObject(named entityName: String, for anchor: ARAnchor){
+        let entity = try! ModelEntity.load(named: entityName)
+        
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        print("anchor entity \(anchorEntity.name)")
+        anchorEntity.addChild(entity);
+        print("Third \(anchor.transform)")
+        self.session.add(anchor: anchor)
+        print("Forth \(anchor.transform)")
+        self.scene.anchors.append(anchorEntity)
+        print("Fifth \(anchor.transform)")
+        //self.scene.addAnchor(anchorEntity)
+        entity.generateCollisionShapes(recursive: true)
+        //self.installGestures([.rotation,.translation], for: entity as! Entity & HasCollision)
+    }
 }
